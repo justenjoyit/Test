f8e8bf89ca9a2b03b81b38272eca8e259dfff597
justenjoyit <693742054@qq.com>
2017-08-16 23:16:57 +0800

    ThreadPoolLib-step2
    ThreadPoolLib-step2

--- ThreadPoolLib.c
+++ ThreadPoolLib.c
@@ -1,5 +1,41 @@
 #include"ThreadPoolLib.h"
 
+void* manage_thread(void*)
+{
+	//取任务列表头的任务
+	pthread_mutex_lock(&_taskList._task_mutex);
+	if(_taskList.getSize()==0)
+		pthread_cond_wait(&thread_manager_cond,&_taskList._task_mutex);
+	Task taskTemp=_taskList.getFront();
+	_taskList.popFront();
+	pthread_mutex_unlock(&_taskList._task_mutex);
+
+	//取空闲链表尾的线程
+	pthread_mutex_lock(&_waitingList._waiting_list_mutex);
+	if(_waitingList.getSize()<=smallestNum)
+	{
+		//扩容，空闲线程值小于最小空闲线程数
+	}else if(_waitingList.getSize()>largestNum)
+	{
+		//缩小，空闲线程数太多
+	}
+
+	//取线程
+	Thread threadTemp=_waitingList.getTop();
+	_waitingList.popTop();
+	pthread_mutex_unlock(&_waitingList._waiting_list_mutex);
+
+	threadTemp.myFunc=taskTemp;
+
+	//将线程和任务加入活动队列并唤醒
+	pthread_mutex_lock(&_activeList._active_list_mutex);
+	_activeList.addThread(threadTemp);
+	pthread_mutex_unlock(&_activeList._active_list_mutex);
+
+	pthread_cond_signal(threadTemp._thread_node_cond);
+}
+
+
 ThreadPool::ThreadPool(int num)
 {
 	tids=new pthread_t[num];
@@ -15,18 +51,17 @@ ThreadPool::~ThreadPool()
 	cout<<"end"<<endl;
 }
 
-void* child_func(void* args)
-{
-	//while(1)
-	//{
-		//wait signal
-	//}
-}
 void ThreadPool::initPool()
 {
 	for(int i=0;i<size;++i)
 	{
-		pthread_create(&tids[i],NULL,child_func,NULL);
+		Thread temp;
+
+		pthread_mutex_lock(&_waitingList._waiting_list_mutex);
+		list<Thread>::iterator iter=_waitingList.addThread(temp);
+		pthread_mutex_unlock(&_waitingList._waiting_list_mutex);
+
+		pthread_create(&tids[i],NULL,child_func,(void*)(iter));
 	}
 }
 
@@ -66,4 +101,20 @@ void TaskList::popFront()
 Task TaskList::getFront()
 {
 	return myTaskList.front();
+}
+
+//初始化系统，创建线程管理和任务管理两个线程
+//初始化互斥量
+void ThreadPool::initSystem()
+{
+	pthread_create(&thread_manager,NULL,manage_thread,NULL);
+	pthread_create(&task_manager,NULL,manage_task,NULL);
+	
+	pthread_mutex_init(&_taskList._task_mutex,NULL);
+	
+	pthread_mutex_init(&_waitingList._waiting_list_mutex,NULL);
+	
+	pthread_mutex_init(&_activeList._active_list_mutex,NULL);
+
+	initPool();
 }
--- ThreadPoolLib.h
+++ ThreadPoolLib.h
@@ -15,12 +15,12 @@ using namespace std;
 class Task
 {
 	public:
-	void* (*myTask)(void*,void*);
+	void* (*myFunc)(void*,void*);
 	void* args;
 	void* rtns;
 	Task(void*(*func)(void*,void*),void*a,void*r)
 	{
-		myTask=func;
+		myFunc=func;
 		args=a;
 		rtns=r;
 	}
@@ -29,30 +29,67 @@ class TaskList
 {
 	private:
 	list<Task> myTaskList;
-	pthread_mutex_t _task_mutex;
 
 	public:
+	pthread_mutex_t _task_mutex;
 	TaskList();
 	void addTask(void*(*func)(void*,void*),void*,void*);
 	void popFront();
 	Task getFront();
 	int getSize();
 };
+
+class Thread
+{
+	public:
+	pthread_t tid;
+	pthread_mutex_t _thread_node_mutex;
+	pthread_cond_t _thread_node_cond;
+	Task _node_task;
+};
+class WaitingThread
+{
+	private:
+	list<Thread> myWaitingThreadList;
+
+	public:
+	pthread_mutex_t _waiting_list_mutex;
+	list<Thread>::iterator addThread(Thread);
+	Thread getTop();
+	void popTop();
+};
+
+class ActiveThread
+{
+	private:
+	list<Thread> myActiveThreadList;
+
+	public:
+	pthread_mutex_t _active_list_mutex;
+	list<Thread>::iterator addThread(Thread);
+	void erase(list<Thread>::iterator);
+};
+
 class ThreadPool
 {
 	private:
 	TaskList _taskList;
-	//WaitingThread _waitingThread;
-	//ActiveThread _activeThread;
-	pthread_t *tids;
+	WaitingThread _waitingList;
+	ActiveThread _activeList;
+	pthread_t thread_manager,task_manager;
+	pthread_cond_t thread_manager_cond;
 	int size;
+	int largestNum,smallestNum;
 
 	public:
 	friend void* child_func(void*);
+	friend void* manage_thread(void*);
+	friend void* manage_task(void*);
 
 	public:
 	ThreadPool(int);
 	~ThreadPool();
 	void initPool();
 	void addTask(void*(*func)(void*,void*),void*,void*);
+	void initSystem();
 };
